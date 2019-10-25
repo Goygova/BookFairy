94b011bea78d64e1e54ec02ed80b232763b22859
Anastasia Goygova <goygova@gmail.com>
2019-10-24 18:01:25 -0700

    Fixed errors

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -6,7 +6,7 @@ class Cards extends React.Component {
 		return (
 			<div className='row col-sm-6'>
 				<div className='cards text-info mr-3' style={{ width: 15 + 'rem' }}>
-					<img className='card-img-top' src={Images.clever} alt='Card image cap'></img>
+					<img className='card-img-top' src={Images.clever} alt='Clever'></img>
 					<div className='card-body'>
 						<h5 className='card-title'>Card title</h5>
 						<p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -16,7 +16,7 @@ class Cards extends React.Component {
 					</div>
 				</div>
 				<div className='cards text-info' style={{ width: 15 + 'rem' }}>
-					<img className='card-img-top' src={Images.melik} alt='Card image cap'></img>
+					<img className='card-img-top' src={Images.melik} alt='Melik-Pashaev'></img>
 					<div className='card-body'>
 						<h5 className='card-title'>Card title</h5>
 						<p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
